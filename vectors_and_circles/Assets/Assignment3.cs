760c7bd30c07558e95304372b8f9889b78545f5a
ljusica <jessiicaceder@hotmail.com>
2021-09-17 11:13:26 +0200

    Fäääärdig

--- vectors_and_circles/Assets/Assignment3.cs
+++ vectors_and_circles/Assets/Assignment3.cs
@@ -4,41 +4,45 @@ using UnityEngine;
 
 public class Assignment3 : ProcessingLite.GP21
 {
-	public Vector2 circlePosition;
-	public float diameter = 2;
-	int colorValue = 0;
-	public Vector2 velocity = new Vector2(0, 0);
+    public Vector2 circlePosition;
+    public float diameter = 2;
+    public Vector2 velocity = new Vector2(0, 0);
+    private float speed = 0.01f;
 
 
-	void Start()
+    void Start()
     {
-		circlePosition.x = Width / 2;
-		circlePosition.y = Height / 2;
-	}
+        circlePosition.x = Width / 2;
+        circlePosition.y = Height / 2;
+    }
 
     void Update()
-	{
-		Background(0);
-		Circle(circlePosition.x, circlePosition.y, diameter);
+    {
+        Background(0);
+        Circle(circlePosition.x, circlePosition.y, diameter);
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = new Vector2(mousePos.x - circlePosition.x, mousePos.y - circlePosition.y);
+        circlePosition += velocity;
 
-		if (Input.GetMouseButton(0))
-		{
-			//circlePosition.x = MouseX;
-			//circlePosition.y = MouseY;
 
-			Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			Vector2 direction = new Vector2(circlePosition.x - mousePos.x, circlePosition.y - mousePos.y);
-			
-			Line(circlePosition.x, circlePosition.y, mousePos.x, mousePos.y);
-			
-			velocity.x = 0.5f * direction.x * -1;
-			velocity.y = 0.5f * direction.y * -1;
-			
+        if (circlePosition.y > Height || circlePosition.y < 0)
+        {
+            velocity.y *= -1;
+        }
 
-			
-			Debug.Log(direction);
-			
+        if (circlePosition.x > Width || circlePosition.x < 0)
+        {
+            velocity.x *= -1;
+        }
 
-		}
-	}
+
+        if (Input.GetMouseButton(0))
+        {
+
+            velocity.x = speed * direction.x;
+            velocity.y = speed * direction.y;
+            Line(circlePosition.x, circlePosition.y, mousePos.x, mousePos.y);
+
+        }
+    }
 }
